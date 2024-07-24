b2cb4971d595e7a5a0cad09af6e38dce0039e572
Yoshida.Eiji <yoshida.eiji@trans-cosmos.co.jp>
2024-07-24 14:25:31 +0900

    1:59:26

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
+import ProductList from "./pages/ProductList";
+import Product from "./pages/Product";
 
 function App() {
   return (
     <>
-      <Home />
+      <Product />
     </>
   );
 }
